29505c0f34e9222bac7de1ec2d9b22ce65306f63
aryan <arcnagpal1234@gmail.com>
2024-07-30 15:07:30 -0400

    Working action server initial commit

---
+++ src/action_serv.cpp
@@ -0,0 +1,106 @@
+#include <functional>
+#include <memory>
+#include <thread>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+#include <std_msgs/msg/string.hpp>
+#include "custom_interfaces/action/huboaction.hpp"
+
+
+class MyActionServer : public rclcpp::Node
+{
+public:
+  using Move = custom_interfaces::action::Huboaction;
+  using GoalHandleMove = rclcpp_action::ServerGoalHandle<Move>;
+
+  explicit MyActionServer(const rclcpp::NodeOptions & options = rclcpp::NodeOptions())
+  : Node("hubo_action_server", options)
+  {
+    using namespace std::placeholders;
+
+    this->action_server_ = rclcpp_action::create_server<Move>(
+      this,
+      "hubo_as",
+      std::bind(&MyActionServer::handle_goal, this, _1, _2),
+      std::bind(&MyActionServer::handle_cancel, this, _1),
+      std::bind(&MyActionServer::handle_accepted, this, _1));
+
+  }
+
+private:
+  rclcpp_action::Server<Move>::SharedPtr action_server_;
+
+  rclcpp_action::GoalResponse handle_goal(
+    const rclcpp_action::GoalUUID & uuid,
+    std::shared_ptr<const Move::Goal> goal)
+  {
+    RCLCPP_INFO(this->get_logger(), "Received goal request with message %s", std::string(goal->msg).c_str());
+    (void)uuid;
+    return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+  }
+
+  rclcpp_action::CancelResponse handle_cancel(
+    const std::shared_ptr<GoalHandleMove> goal_handle)
+    
+  {
+    RCLCPP_INFO(this->get_logger(), "Received request to cancel goal");
+    (void)goal_handle;
+    return rclcpp_action::CancelResponse::ACCEPT;
+  }
+
+  void handle_accepted(const std::shared_ptr<GoalHandleMove> goal_handle)
+  {
+    using namespace std::placeholders;
+    // this needs to return quickly to avoid blocking the executor, so spin up a new thread
+    std::thread{std::bind(&MyActionServer::execute, this, _1), goal_handle}.detach();
+  }
+
+  void execute(const std::shared_ptr<GoalHandleMove> goal_handle)
+  {
+    RCLCPP_INFO(this->get_logger(), "Executing goal");
+    rclcpp::Rate loop_rate(1);
+    const auto goal = goal_handle->get_goal();
+    auto feedback = std::make_shared<Move::Feedback>();
+    auto result = std::make_shared<Move::Result>();
+    /*
+    for (int i = 1; (i < goal->order) && rclcpp::ok(); ++i) {
+      // Check if there is a cancel request
+      if (goal_handle->is_canceling()) {
+        result->status = false;
+        goal_handle->canceled(result);
+        RCLCPP_INFO(this->get_logger(), "Goal canceled");
+        return;
+      }
+      // Update sequence
+      // Publish feedback
+      goal_handle->publish_feedback(feedback);
+      RCLCPP_INFO(this->get_logger(), "Publish feedback");
+
+      loop_rate.sleep();
+    }
+    */
+    // Check if goal is done
+    if (rclcpp::ok()) {
+      result->status = true;
+      goal_handle->succeed(result);
+      RCLCPP_INFO(this->get_logger(), "Goal succeeded");
+    }
+    
+  }
+};  // class MyActionServer
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+
+  auto action_server = std::make_shared<MyActionServer>();
+    
+  rclcpp::executors::MultiThreadedExecutor executor;
+  executor.add_node(action_server);
+  executor.spin();
+
+  rclcpp::shutdown();
+  return 0;
+}
